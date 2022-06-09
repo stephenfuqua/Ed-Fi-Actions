078738821add84dc1a209f80db636d994368fec4
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2022-06-09 16:14:53 +0000

    [DEVOPS-97] Correct some typos (#13)
    * Insert missing file headers
    
    * Allow local execution
    
    * Typo corrections

---
+++ action-allowedlist/src/Invoke-LocalScan.ps1
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+<#
+.SYNOPSIS
+    Use for "localhost" execution of the approved Actions review.
+#>
+
+param(
+    # The repository directory to scan
+    [Parameter(Mandatory=$True)]
+    [string]
+    $RepoDirectory
+)
+
+
+Import-Module ./action-allowedlist.psm1 -DisableNameChecking
+
+$actionsFound = LoadAllUsedActions -RepoPath $RepoDirectory
+$unapproved = CheckIfActionsApproved -outputs $actionsFound -approvedPath "$($PSScriptRoot)/../approved.json"
+
+$unapproved | Out-Host
--- action-allowedlist/src/dependencies.ps1
+++ action-allowedlist/src/dependencies.ps1
@@ -1,7 +1,20 @@
-$env:PSModulePath += ":/root/.local/share/powershell/Modules"
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+$runningInGitHub = $env:GITHUB_ACTIONS -eq $true
 
 try {
-    Import-Module /src/powershell-yaml/ -Force
+
+    if ($runningInGitHub) {
+        $env:PSModulePath += ":/root/.local/share/powershell/Modules"
+        Import-Module /src/powershell-yaml/ -Force
+    }
+    else {
+        $env:PSModulePath += "$($PSScriptRoot)/../dep/powershell-yaml"
+        Import-Module "$($PSScriptRoot)/../dep/powershell-yaml" -Force
+    }
 }
 catch {
     Write-Warning "Error during importing of the yaml module needed for parsing"

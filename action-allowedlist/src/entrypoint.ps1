a178b3043bfd9f5c85aa0dfa07994fad1c7b63ef
Stephen A. Fuqua <stephen@safnet.com>
2022-09-27 15:13:27 -0500

    Format files

--- action-allowedlist/src/entrypoint.ps1
+++ action-allowedlist/src/entrypoint.ps1
@@ -14,7 +14,8 @@ Write-Output "::set-output name=actions::'$jsonObject'"
 if ($unapproved.Count -gt 0) {
     Write-Error "Repo contains unapproved actions!"
     exit 1
-}else{
+}
+else {
     #Write-Host "All actions were approved!"
     exit 0
 }

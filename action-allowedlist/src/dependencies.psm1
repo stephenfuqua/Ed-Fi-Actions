916e5b1d7216563c264c2ab13dfc8e0390122c11
Stephen A. Fuqua <stephen@safnet.com>
2023-01-04 10:31:18 -0600

    Change to a module

---
+++ action-allowedlist/src/dependencies.psm1
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+$runningInGitHub = $env:GITHUB_ACTIONS -eq $true
+
+if ($runningInGitHub) {
+    $env:PSModulePath += ":/root/.local/share/powershell/Modules"
+    Import-Module /src/powershell-yaml/ -Force
+}
+else {
+    $env:PSModulePath += "$($PSScriptRoot)/../dep/powershell-yaml"
+    Import-Module "$($PSScriptRoot)/../dep/powershell-yaml" -Force
+}
